683c8e38dcc49d2ef5cdd3099e7abd82f2ae3b9d
bux <33053305>
2021-05-07 13:21:30 +0300

    rebuild backend

--- src/rembg/cmd/server.py
+++ src/rembg/cmd/server.py
@@ -4,24 +4,36 @@ import argparse
 from io import BytesIO
 from urllib.parse import unquote_plus
 from urllib.request import urlopen
-
-from flask import Flask, request, send_file
+import base64
+from flask import Flask, request, send_file, jsonify
 from waitress import serve
 
 from ..bg import remove
+from PIL import Image
 
 app = Flask(__name__)
 
+def img_to_base64_str(buf):
+    img = Image.open(buf)
+    img.save(buf, format="PNG")
+    buf.seek(0)
+    img_byte = buf.getvalue()
+    img_str = "data:image/png;base64," + base64.b64encode(img_byte).decode()
+    return img_str
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     file_content = ""
 
-    if request.method == "POST":
-        if "file" not in request.files:
-            return {"error": "missing post form param 'file'"}, 400
-
-        file_content = request.files["file"].read()
+    if request.method == "POST" and "image_data" in request.json:
+        json_data = request.get_json()
+        image_data = json_data['image_data']
+        file_content = base64.b64decode(image_data)
+        alpha_matting = json_data['a'] or False
+        af = json_data['af'] or 240
+        ab = json_data['ab'] or 10
+        ae = json_data['ae'] or 10
+        az = json_data['az'] or 1000
 
     if request.method == "GET":
         url = request.args.get("url", type=str)
@@ -29,15 +41,14 @@ def index():
             return {"error": "missing query param 'url'"}, 400
 
         file_content = urlopen(unquote_plus(url)).read()
+        alpha_matting = "a" in request.values
+        af = request.values.get("af", type=int, default=240)
+        ab = request.values.get("ab", type=int, default=10)
+        ae = request.values.get("ae", type=int, default=10)
+        az = request.values.get("az", type=int, default=1000)
 
     if file_content == "":
         return {"error": "File content is empty"}, 400
-
-    alpha_matting = "a" in request.values
-    af = request.values.get("af", type=int, default=240)
-    ab = request.values.get("ab", type=int, default=10)
-    ae = request.values.get("ae", type=int, default=10)
-    az = request.values.get("az", type=int, default=1000)
 
     model = request.args.get("model", type=str, default="u2net")
     model_path = os.environ.get(
@@ -52,19 +63,19 @@ def index():
         return {"error": f"invalid query param 'model'. Available options are {model_choices}"}, 400
 
     try:
-        return send_file(
-            BytesIO(
-                remove(
-                    file_content,
-                    model_name=model,
-                    alpha_matting=alpha_matting,
-                    alpha_matting_foreground_threshold=af,
-                    alpha_matting_background_threshold=ab,
-                    alpha_matting_erode_structure_size=ae,
-                    alpha_matting_base_size=az,
-                )
-            ),
-            mimetype="image/png",
+        file_without_background = remove(
+                                         file_content,
+                                         model_name=model,
+                                         alpha_matting=alpha_matting,
+                                         alpha_matting_foreground_threshold=af,
+                                         alpha_matting_background_threshold=ab,
+                                         alpha_matting_erode_structure_size=ae,
+                                         alpha_matting_base_size=az,
+                                     )
+        file_data = BytesIO(file_without_background)
+        encoded = img_to_base64_str(file_data)
+        return jsonify(
+            converted_image=encoded,
         )
     except Exception as e:
         app.logger.exception(e, exc_info=True)
